5a8c11a8dbb212a0c05c6a9a89c966a847eb566c
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-05-28 13:28:29 +0300

    ornek json dosyası olusturup cagırdık

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import db from "../../db/productsDb.json";
 
 const Home = () => {
   const naviGate = useNavigate();
   const gitUrunler = (categoryName) => {
     naviGate(`products/${categoryName}`);
   };
+  console.log(db);
   return (
     <div>
       <h1>Home</h1>
