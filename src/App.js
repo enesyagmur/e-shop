9b42a691eda6240410921027e20581bbac068620
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-05-28 12:56:55 +0300

    router yapisi olusturuldu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:category" element={<Products />} />
+          <Route path="/detail/:product" element={<Detail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
