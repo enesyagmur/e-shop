afde766d559691d4f824c404a60d9065096d10a8
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-05-31 11:05:36 +0300

    tıklanan kaegoriye sahip urunleri gosterme

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import db from "../../db/productsDb.json";
 
 const Products = () => {
-  const categoryName = useParams();
-  console.log(categoryName.category);
+  const [data, setData] = useState("");
+  const params = useParams();
+  const categoryName = params.category;
+
+  useEffect(() => {
+    const newDb = db.filter((item) => item.category === categoryName);
+    setData(newDb);
+  }, []);
+
   // const naviGate = useNavigate();
   // // const gitDetay = (productName) => {
   // //   naviGate(`/detail/${productName}`);
   // // };
 
-  return (
-    <div>
-      <h1>{categoryName.category}</h1>
-    </div>
-  );
+  return <div>{data ? data.map((item) => <h1>{item.name}</h1>) : null}</div>;
 };
 
 export default Products;
