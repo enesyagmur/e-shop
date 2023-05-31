f6d0d09c758b3fcf11f776e5c6dd98f1677ce60d
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-05-31 13:15:48 +0300

    artan ve azalan butonları ve islevleri yapıldı

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -8,9 +8,13 @@ import Filter from "./filter";
 const Products = () => {
   const [data, setData] = useState("");
   const [input, setInput] = useState("");
-  const [selectCategory, setselectCategory] = useState("hepsi");
+  const [sorting, setSorting] = useState("");
   const params = useParams();
   let category = params.category;
+
+  useEffect(() => {
+    setData(db);
+  }, [sorting]);
 
   useEffect(() => {
     if (category !== "hepsi") {
@@ -21,17 +25,27 @@ const Products = () => {
     }
   }, []);
 
-  const filter = (categoryName) => {
+  const categoryFilter = (categoryName) => {
     const newDb = db.filter((item) => item.category === categoryName);
     setData(newDb);
   };
 
+  const sort = (type) => {
+    if (type === "artan") {
+      db.sort((a, b) => a.price - b.price);
+      setSorting("fiyat artan şeklinde sıralandı");
+    } else if (type === "azalan") {
+      db.sort((a, b) => b.price - a.price);
+      setSorting("fiyat azalan şeklinde sıralandı");
+    }
+  };
+
   return (
     <div className="products">
-      <SearchBar />
+      <SearchBar sort={sort} />
       <main>
         {" "}
-        {category === "hepsi" ? <Filter filtered={filter} /> : null}
+        {category === "hepsi" ? <Filter filtered={categoryFilter} /> : null}
         <div className="productsList">
           {data
             ? data.map((item) => (
--- src/pages/products/searchBar.jsx
+++ src/pages/products/searchBar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-const SearchBar = ({ setInput }) => {
+const SearchBar = ({ setInput, sort }) => {
   const sendInput = (value) => {
     setInput(value);
   };
   return (
     <div className="searchBar">
+      <button onClick={() => sort("artan")}>Artan</button>
       <input type="text" onChange={(e) => sendInput(e.target.value)} />
+      <button onClick={() => sort("azalan")}>Azalan</button>
     </div>
   );
 };
