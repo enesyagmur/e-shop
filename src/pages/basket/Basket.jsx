79ead3716103ad5a04dcfe3d93685b4dabd6f4eb
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-06-06 19:54:46 +0300

    sepet sayfasında urunleri listeleme yapıldı

--- src/pages/basket/Basket.jsx
+++ src/pages/basket/Basket.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "../../style/sepet.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Basket = () => {
-  return <div>baskett</div>;
+  const basket = useSelector((state) => state.basket.basketProducts);
+
+  return (
+    <div className="sepet">
+      <div className="basketProducts">
+        {basket
+          ? basket.map((item) => (
+              <div className="basketProduct">
+                <img src={item.productImage} />
+                <p>{item.productName}</p>
+                <p>{item.productPrice} TL</p>
+              </div>
+            ))
+          : null}
+      </div>
+    </div>
+  );
 };
 
 export default Basket;
