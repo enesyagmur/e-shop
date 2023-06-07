c2c14ce3d0fb1acaf0a1c02139622b4e042b3af3
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-06-07 10:50:00 +0300

    genel style islemleri

--- src/pages/products/searchBar.jsx
+++ src/pages/products/searchBar.jsx
@@ -1,15 +1,8 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 const SearchBar = ({ search, sort }) => {
-  const navigate = useNavigate();
-  const goHome = () => {
-    navigate(`/`);
-  };
-
   return (
     <div className="searchBar">
-      <button onClick={goHome}>Home</button>
       <button onClick={() => sort("artan")}>Artan</button>
       <input type="text" onChange={(e) => search(e.target.value)} />
       <button onClick={() => sort("azalan")}>Azalan</button>
